a92cb2835942516c928677e14cb8ab464fb4c553
kgiszewski <kevin@giszewski.com>
2018-12-21 09:37:09 -0500

    Add in ErrorHelper

---
+++ src/G42.Helpers/Exceptions/ErrorHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace G42.Helpers.Exceptions
+{
+    public static class ErrorHelper
+    {
+        public static string GetInnerExceptions(System.Exception exception)
+        {
+            var sb = new StringBuilder();
+
+            if (exception != null)
+            {
+                sb.AppendLine(exception.Message);
+
+                GetInnerExceptions(exception.InnerException);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
